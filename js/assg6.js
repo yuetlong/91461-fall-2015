68d3f0478cf1e7c71244f51dee8d6d60adc272c3
yuetlong <yuetlong@icloud.com>
2015-11-08 11:10:06 -0500

    modified assg6.js

--- js/assg6.js
+++ js/assg6.js
@@ -1,53 +1,77 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Name    : js/assg6.js
+    Author  : Yuet Long Leung
+    Email   : YuetLong_Leung@student.uml.edu
+    Date    : Nov 5, 2015
+    Copyright (c) 2015 by Yuet Long Leung.  All rights reserved.
  */
-
 
 function submitForm(){
     var inputs = document.getElementById("inputs").elements;
+    var error_message = "";
+    lb = ["Row min", "Row max", "Column min", "Column max"];
     
-    if(inputs[0].value === "")
-        return window.alert("Row min is empty.");
+    for (i = 0; i < 4; i++){
+        if(inputs[i].value === ""){
+            error_message += "<p>" + lb[i] + " is empty.</p>";
+        }
+        else if(/[A-Za-z]/.test(inputs[i].value)){
+            error_message += "<p>" + lb[i] + " contains letters.</p>";
+        }
+        else if(/\s/.test(inputs[i].value)){
+            error_message += "<p>" + lb[i] + " contains whitespace.</p>";
+        }
+    }
     
-    if(inputs[1].value === "")
-        return window.alert("Row max is empty.");
-    
-    if(inputs[2].value === "")
-        return window.alert("Column min is empty.");
-    
-    if(inputs[3].value === "")
-        return window.alert("Column max is empty.");
-    
+    /* now we know the string value is safe to parse to Integers */
     var rmin = parseInt(inputs[0].value);
     var rmax = parseInt(inputs[1].value);
     var cmin = parseInt(inputs[2].value);
     var cmax = parseInt(inputs[3].value);
     
     if(rmin >= rmax)
-        return window.alert("Row min is bigger or equal to row max.");
-    
+        error_message += "<p>Row min is bigger or equal to row max.</p>";
+        
     if(cmin >= cmax)
-        return window.alert("Column min is bigger or equal to column max.");
+        error_message += "<p>Column min is bigger or equal to column max.</p>";
     
-    builtATable(rmin,rmax,cmin,cmax);
+    /* we either output error messages or the table */
+    if (error_message !== ""){
+        document.getElementById("error_message").innerHTML = error_message;
+        document.getElementById("mytable").innerHTML = "";
+        return;
+    }
+    else{
+        document.getElementById("error_message").innerHTML = "";
+        builtATable(rmin, rmax, cmin, cmax);
+    }
 }
 
 function builtATable(rmin, rmax, cmin, cmax){
-    var str = "<tr><td></td>";
+    var str = "<tr><td></td>"; /* first block has no use, will take it out of sight using CSS */
+    
+    /* row multiplicands */
     for (i = cmin; i <= cmax; i++){
         str += "<td>" + i + "</td>";
     }
+    
+    /* end tag */
     str += "</tr>";
+    
+    
     for (i = rmin; i <= rmax; i++){
+        /* column multipliers */
         str += "<tr><td>" + i + "</td>";
+        
+        /* contents */
         for (j = cmin; j <= cmax; j++){
             str += "<td> " + (i*j) + "</td>";
         }
+        
+        /* end tag */
         str += "</tr>";
     }
     
+    /* update the table */
     document.getElementById("mytable").innerHTML = str;
-    //document.getElementById("inputs").reset();
 }
