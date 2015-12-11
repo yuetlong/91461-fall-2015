61b5574d64c5c3fb913878106718dd7153d04596
yuetlong <yuetlong@icloud.com>
2015-12-10 23:07:10 -0500

    fixed get_score

--- js/assg9.js
+++ js/assg9.js
@@ -15,76 +15,72 @@ http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript
 
 */
 
-// pieces[?][1] is the count , pieces[?][2] is the value.
+var piece_value = {
+  A:1,
+  B:3,
+  C:3,
+  D:2,
+  E:1,
+  F:4,
+  G:2,
+  H:4,
+  I:1,
+  J:8,
+  K:5,
+  L:1,
+  M:3,
+  N:1,
+  O:1,
+  P:3,
+  Q:10,
+  R:1,
+  S:1,
+  T:1,
+  U:1,
+  V:4,
+  W:4,
+  X:8,
+  Y:4,
+  Z:10,
+  b:0
+};
 
+var piece_count = {
+  A:9,
+  B:2,
+  C:2,
+  D:4,
+  E:12,
+  F:2,
+  G:3,
+  H:2,
+  I:9,
+  J:1,
+  K:1,
+  L:4,
+  M:2,
+  N:5,
+  O:8,
+  P:2,
+  Q:1,
+  R:6,
+  S:4,
+  T:6,
+  U:4,
+  V:2,
+  W:2,
+  X:1,
+  Y:2,
+  Z:1,
+  b:2
+};
 
-var pieces = [
-  ["A",1,9],
-  ["B",3,2],
-  ["C",3,2],
-  ["D",2,4],
-  ["E",1,12],
-  ["F",4,2],
-  ["G",2,3],
-  ["H",4,2],
-  ["I",1,9],
-  ["J",8,1],
-  ["K",5,1],
-  ["L",1,4],
-  ["M",3,2],
-  ["N",1,5],
-  ["O",1,8],
-  ["P",3,2],
-  ["Q",10,1],
-  ["R",1,6],
-  ["S",1,4],
-  ["T",1,6],
-  ["U",1,4],
-  ["V",4,2],
-  ["W",4,2],
-  ["X",8,1],
-  ["Y",4,2],
-  ["Z",10,1],
-  ["blank",0,2]
-];
-
-function get_count(letter){
-  for (i = 0 ; i < 27; i++){
-    if (pieces[i][0] == letter){
-      return pieces[i][1];
-    }
-  }
-}
-
-function get_value(letter){
-  for (i = 0 ; i < 27; i++){
-    if (pieces[i][0] == letter){
-      return pieces[i][2];
-    }
-  }
-}
-
-var letters_on_board = new Array(15);
-for (i = 0; i < 15 ; i ++){
-  letters_on_board[i] = new Array(15);
-}
-
-// I am going to sequentially insert all the pieces into the array then shuffle it.
-
-var p_gen = new Array();
-
-for (i = 0; i < pieces.length ; i++){
-  for (j = 0; j < pieces[i][2]; j++){
-    p_gen.push(pieces[i][0]);
-  }
-}
-
-var total_score = 0;
-
-// this is the de facto shuffle function from stackoverflow
+/******************************************************************************************
+                      de facto shuffle function from stackoverflow
+*******************************************************************************************/
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -102,7 +98,124 @@ function shuffle(array) {
   return array;
 }
 
+/******************************************************************************************
+    I am going to sequentially insert all the pieces into the array then shuffle it.
+*******************************************************************************************/
+
+var p_gen = new Array();
+for (key in piece_count){
+  for(i = 0 ; i < piece_count[key]; i++){
+    p_gen.push(key);
+  }
+}
 shuffle(p_gen);
+
+/******************************************************************************************
+      Initialize 2D array for recording what tile contains which piece.
+*******************************************************************************************/
+
+var letters_on_board = new Array(15);
+for (i = 0; i < 15 ; i++){
+  letters_on_board[i] = new Array(15);
+  for (j = 0 ; j < 15; j++){
+    letters_on_board[i][j] = "empty";
+  }
+}
+
+/******************************************************************************************
+      Make a function for calculating the total score.
+*******************************************************************************************/
+
+
+function get_score(){
+
+  total_score = 0;
+
+  score_on_board = new Array(15);
+  for(i = 0; i < 15; i++){
+    score_on_board[i] = new Array(15);
+  }
+
+  /******************************************************************************************
+        Make a function for the multipling word score tiles.
+  *******************************************************************************************/
+  function multiply_word(i, j, m){
+    // go up ... (and itself)
+    a = i;
+
+    while(a >= 0 && letters_on_board[a][j] != "empty")
+        score_on_board[a--][j] *= m;
+
+    // go down
+    a = i + 1;
+
+    while(a <= 14 && letters_on_board[a][j] != "empty")
+        score_on_board[a++][j] *= m;
+
+    // go left
+    b = j;
+
+    while(b >= 0 && letters_on_board[i][b] != "empty")
+        score_on_board[i][b--] *= m;
+
+    // go right
+    b = j + 1;
+
+    while(b <= 14 && letters_on_board[i][b] != "empty")
+        score_on_board[i][b++] *= m;
+  } // end function multiply_word(i, j, m){
+
+
+    /******************************************************************************************
+          Main loop for calculating the scores of individual tiles.
+    *******************************************************************************************/
+  for (i = 0; i < 15; i++){
+    for (j = 0 ; j < 15 ; j++){
+      if (letters_on_board[i][j] != "empty"){
+        score_on_board[i][j] = piece_value[letters_on_board[i][j]];
+
+        if      (((i == 0 || i == 7 || i == 14) && (j == 3 || j == 11))
+              || ((i == 2 || i == 12)           && (j == 6 || j == 8 ))
+              || ((i == 3 || i == 11)           && (j == 0 || j == 7 || j == 14))
+              || ((i == 6 || i == 8 )           && (j == 2 || j == 6 || j == 8 || j == 12))
+              ||  (i == 7 && j == 7)){
+          score_on_board[i][j] *= 2;
+        }
+        else if (((i == 1 || i == 13) && (j == 5 || j == 9))
+              || ((i == 5 || i == 9) &&  (j == 1 || j == 5 || j == 9 || j == 13))){
+          score_on_board[i][j] *= 3;
+        }g
+        if ((i == 0 || i == 7 || i == 14) && (j == 0 || j == 7 || j == 14) && !(i == 7 && j == 7)){
+          multiply_word(i,j,3);
+        }
+
+        else if (((i == 1 || i == 13) && (j == 1 || j == 13))
+             ||  ((i == 2 || i == 12) && (j == 2 || j == 12))
+             ||  ((i == 3 || i == 11) && (j == 3 || j == 11))
+             ||  ((i == 4 || i == 10) && (j == 4 || j == 10))){
+          multiply_word(i,j,2);
+        }
+      }
+      else{
+        score_on_board[i][j] = 0;
+      }
+    }
+  }
+
+
+    /******************************************************************************************
+        Make a function for the multipling word score tiles.
+    *******************************************************************************************/
+
+  for(i = 0; i < 15; i++){
+    for(j = 0; j < 15; j++){
+      total_score += score_on_board[i][j];
+    }
+  }
+
+  return total_score;
+
+} // function get_score(){
 
 $(document).ready(function() {
 
@@ -118,7 +231,7 @@ $(document).ready(function() {
     new_piece.setAttribute('is_on_board',false);
 
     new_pic = document.createElement('img');
-    new_pic.setAttribute('src','img/st/' + p_gen[i][0] + '.jpg');
+    new_pic.setAttribute('src','img/st/' + p_gen[i] + '.jpg');
     new_pic.setAttribute('height','50');
     new_pic.setAttribute('width','50');
 
@@ -230,6 +343,8 @@ $(document).ready(function() {
 
       // store the letter into the array
       letters_on_board[$(this).attr('row')][$(this).attr('column')] = ui.draggable.attr('letter');
+
+      console.log(get_score());
     },
 
 
@@ -245,7 +360,7 @@ $(document).ready(function() {
         $(this).droppable('option','accept',".piece");
 
         // clear the array element
-        letters_on_board[$(this).attr('row')][$(this).attr('column')] = undefined;
+        letters_on_board[$(this).attr('row')][$(this).attr('column')] = "empty";
       }
     }
   }); // end  $(".tile").droppable({
@@ -261,4 +376,9 @@ $(document).ready(function() {
       ui.draggable.draggable('option','revert',false);
     }
   }) // end $("rack").droppable({
+
+    /******************************************************************************************
+                                 Configure the get score button
+    *******************************************************************************************/
+
 }); // end $(document).ready(function() {
